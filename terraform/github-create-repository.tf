c06606e63bd9cc30c2afa09317b63d8c7a6b3138
Petr Ruzicka <petr.ruzicka@gmail.com>
2021-12-27 17:18:01 +0100

    Add Terraform for creating GitHub repository

---
+++ terraform/github-create-repository.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_version = "~> 1"
+  required_providers {
+    github = {
+      source  = "hashicorp/github"
+      version = "~> 4"
+    }
+  }
+}
+
+################################################################################
+# Providers
+################################################################################
+
+provider "github" {}
+
+################################################################################
+# GitHub Repository
+################################################################################
+
+resource "github_repository" "repository" {
+  name                   = "test-repo"
+  description            = "🤖 This repo was auto-generated with Terraform 🤖"
+  topics                 = ["blog", "database", "reports", "plugin"]
+  homepage_url           = "https://petr.ruzicka.dev"
+  has_wiki               = false
+  has_projects           = false
+  allow_merge_commit     = false
+  delete_branch_on_merge = true
+  auto_init              = true
+  license_template       = "apache-2.0"
+}
