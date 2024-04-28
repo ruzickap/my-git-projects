89b8a8a90ca2081f4ea5bbdeeb57bfd1f3430c2f
Petr Ruzicka <petr.ruzicka@gmail.com>
2024-04-28 07:37:34 +0200

    refactor(gha): updates + remove default gha files from tf (#128)

--- opentofu/github-create-repository.tf
+++ opentofu/github-create-repository.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     github = {
       source  = "integrations/github"
-      version = "~> 5"
+      version = "~> 6"
     }
   }
 }
@@ -31,25 +31,6 @@ variable "my_renovate_github_private_key" {
 }
 
 ################################################################################
-# Variables
-################################################################################
-
-locals {
-  github_repository_default_files = [
-    ".github/CODEOWNERS",
-    ".github/ISSUE_TEMPLATE/bug_report.md",
-    ".github/ISSUE_TEMPLATE/config.yml",
-    ".github/ISSUE_TEMPLATE/proposal.md",
-    ".github/renovate.json5",
-    ".github/workflows/mega-linter.yml",
-    ".github/workflows/release-please.yml",
-    ".github/workflows/renovate.yml",
-    ".github/workflows/semantic-pull-request.yml",
-    ".github/workflows/stale.yml",
-  ]
-}
-
-################################################################################
 # GitHub Repository
 ################################################################################
 
@@ -61,16 +42,17 @@ resource "github_repository" "repository" {
   allow_update_branch    = true
   auto_init              = true
   delete_branch_on_merge = true
-  description            = "Multicloud+Multitenant+Multicluster Managed K8s Cluster installation using OpenTofu, GitHub Actions and ArgoCD"
-  has_issues             = true
-  has_projects           = false
-  has_wiki               = false
-  homepage_url           = "https://petr.ruzicka.dev"
-  license_template       = "apache-2.0"
-  name                   = "k8s-tf-gitops"
-  topics                 = ["opentofu", "eks", "aks", "argocd", "multicluster", "multitenant", "k8s", "gitops", "multicloud", "github-actions"]
-  visibility             = "public"
-  vulnerability_alerts   = true
+  description            = "K8s Hacking Notes"
+  # gitignore_template     = "Terraform"
+  has_issues           = true
+  has_projects         = false
+  has_wiki             = false
+  homepage_url         = "https://petr.ruzicka.dev"
+  license_template     = "apache-2.0"
+  name                 = "k8s-hacking-notes"
+  topics               = ["k8s", "hacking", "notes"]
+  visibility           = "public"
+  vulnerability_alerts = true
   security_and_analysis {
     secret_scanning {
       status = "enabled"
@@ -106,20 +88,16 @@ resource "github_actions_secret" "my_renovate_github_private_key" {
   plaintext_value = var.my_renovate_github_private_key
 }
 
-resource "github_repository_file" "github-files" {
-  # for_each   = local.github_repository_default_files
-  # for_each   = { for file in local.github_repository_default_files : file.name => file}
-  for_each   = toset(local.github_repository_default_files)
-  file       = each.value
-  repository = github_repository.repository.name
-  content    = file("../${each.value}")
-}
-
 ################################################################################
 # Outputs
 ################################################################################
 
 output "github_ssh_clone_url" {
   description = "GitHub SSH Clone URL"
+  value       = github_repository.repository.ssh_clone_url
+}
+
+output "github_ssh_clone_url_commands" {
+  description = "GitHub SSH Clone URL Commands"
   value       = github_repository.repository.ssh_clone_url
 }
