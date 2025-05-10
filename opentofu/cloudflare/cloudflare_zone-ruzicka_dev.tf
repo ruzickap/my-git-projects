aeaace71950920a468700b0d79e8b2d5c9d9556a
Petr Ruzicka <petr.ruzicka@gmail.com>
2025-05-10 08:30:16 +0200

    feat: install tfcmt and update DNS record names for clarity (#189)

--- opentofu/cloudflare/cloudflare_zone-ruzicka_dev.tf
+++ opentofu/cloudflare/cloudflare_zone-ruzicka_dev.tf
@@ -16,7 +16,7 @@ resource "cloudflare_dns_record" "cname_blog_ruzicka_dev" {
   zone_id = cloudflare_zone.ruzicka_dev.id
   comment = "ruzickap.github.io - personal blog 🏠 (https://github.com/ruzickap/ruzickap.github.io)"
   content = "ruzickap.github.io"
-  name    = "blog"
+  name    = "blog.ruzicka.dev"
   proxied = true
   ttl     = 1
   type    = "CNAME"
@@ -26,7 +26,7 @@ resource "cloudflare_dns_record" "cname_petr_ruzicka_dev" {
   zone_id = cloudflare_zone.ruzicka_dev.id
   comment = "Personal page (https://github.com/ruzickap/petr.ruzicka.dev)"
   content = "ruzickap.github.io"
-  name    = "petr"
+  name    = "petr.ruzicka.dev"
   proxied = false
   ttl     = 1
   type    = "CNAME"
@@ -36,7 +36,7 @@ resource "cloudflare_dns_record" "cname_ruzicka_dev" {
   zone_id = cloudflare_zone.ruzicka_dev.id
   comment = "Personal page (https://github.com/ruzickap/petr.ruzicka.dev)"
   content = "petr.ruzicka.dev"
-  name    = "@"
+  name    = "ruzicka.dev"
   proxied = true
   ttl     = 1
   type    = "CNAME"
@@ -46,7 +46,7 @@ resource "cloudflare_dns_record" "cname_stats_ruzickap_github_io_ruzicka_dev" {
   zone_id = cloudflare_zone.ruzicka_dev.id
   comment = "GoatCounter for Petr's Blog (https://www.goatcounter.com/help/faq#custom-domain)"
   content = "ruzickap-github-io.goatcounter.com"
-  name    = "stats-ruzickap-github-io"
+  name    = "stats-ruzickap-github-io.ruzicka.dev"
   proxied = false
   ttl     = 1
   type    = "CNAME"
@@ -56,7 +56,7 @@ resource "cloudflare_dns_record" "cname_www_ruzicka_dev" {
   zone_id = cloudflare_zone.ruzicka_dev.id
   comment = "Personal page (https://github.com/ruzickap/petr.ruzicka.dev)"
   content = "petr.ruzicka.dev"
-  name    = "www"
+  name    = "www.ruzicka.dev"
   proxied = true
   ttl     = 1
   type    = "CNAME"
@@ -65,7 +65,7 @@ resource "cloudflare_dns_record" "cname_www_ruzicka_dev" {
 resource "cloudflare_dns_record" "mx1_ruzicka_dev" {
   zone_id  = cloudflare_zone.ruzicka_dev.id
   content  = "route1.mx.cloudflare.net"
-  name     = "@"
+  name     = "ruzicka.dev"
   priority = 72
   proxied  = false
   ttl      = 1
@@ -76,7 +76,7 @@ resource "cloudflare_dns_record" "mx2_ruzicka_dev" {
   zone_id  = cloudflare_zone.ruzicka_dev.id
   comment  = "Used by CloudFlare Email Routing"
   content  = "route2.mx.cloudflare.net"
-  name     = "@"
+  name     = "ruzicka.dev"
   priority = 11
   proxied  = false
   ttl      = 1
@@ -87,7 +87,7 @@ resource "cloudflare_dns_record" "mx3_ruzicka_dev" {
   zone_id  = cloudflare_zone.ruzicka_dev.id
   comment  = "Used by CloudFlare Email Routing"
   content  = "route3.mx.cloudflare.net"
-  name     = "@"
+  name     = "ruzicka.dev"
   priority = 20
   proxied  = false
   ttl      = 1
@@ -97,7 +97,7 @@ resource "cloudflare_dns_record" "mx3_ruzicka_dev" {
 resource "cloudflare_dns_record" "spf_txt_ruzicka_dev" {
   zone_id = cloudflare_zone.ruzicka_dev.id
   content = "\"v=spf1 include:_spf.mx.cloudflare.net ~all\""
-  name    = "@"
+  name    = "ruzicka.dev"
   proxied = false
   ttl     = 1
   type    = "TXT"
@@ -106,7 +106,7 @@ resource "cloudflare_dns_record" "spf_txt_ruzicka_dev" {
 resource "cloudflare_dns_record" "txt_cf2024_1_domainkey_ruzicka_dev" {
   zone_id = cloudflare_zone.ruzicka_dev.id
   content = "\"v=DKIM1; h=sha256; k=rsa; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAiweykoi+o48IOGuP7GR3X0MOExCUDY/BCRHoWBnh3rChl7WhdyCxW3jgq1daEjPPqoi7sJvdg5hEQVsgVRQP4DcnQDVjGMbASQtrY4WmB1VebF+RPJB2ECPsEDTpeiI5ZyUAwJaVX7r6bznU67g7LvFq35yIo4sdlmtZGV+i0H4cpYH9+3JJ78k\" \"m4KXwaf9xUJCWF6nxeD+qG6Fyruw1Qlbds2r85U9dkNDVAS3gioCvELryh1TxKGiVTkg4wqHTyHfWsp7KD3WQHYJn0RyfJJu6YEmL77zonn7p2SRMvTMP3ZEXibnC9gz3nnhR6wcYL8Q7zXypKTMD58bTixDSJwIDAQAB\""
-  name    = "cf2024-1._domainkey"
+  name    = "cf2024-1._domainkey.ruzicka.dev"
   proxied = false
   ttl     = 1
   type    = "TXT"
@@ -116,7 +116,7 @@ resource "cloudflare_dns_record" "txt_github_pages_challenge_ruzickap_ruzicka_de
   zone_id = cloudflare_zone.ruzicka_dev.id
   comment = "GitHub Pages custom domain verification for ruzicka.dev domain (https://github.com/settings/pages)"
   content = "\"d2344c41a13a4a413464c8d118fa60\""
-  name    = "_github-pages-challenge-ruzickap"
+  name    = "_github-pages-challenge-ruzickap.ruzicka.dev"
   proxied = false
   ttl     = 1
   type    = "TXT"
