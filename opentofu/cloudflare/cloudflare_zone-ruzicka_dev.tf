b1577224891559fd4e550f7b66a7db77909b059d
Petr Ruzicka <petr.ruzicka@gmail.com>
2025-10-03 06:39:37 +0200

    fix: Enhance comments in Cloudflare zone configurations for clarity

--- opentofu/cloudflare/cloudflare_zone-ruzicka_dev.tf
+++ opentofu/cloudflare/cloudflare_zone-ruzicka_dev.tf
@@ -37,7 +37,7 @@ locals {
     "mx1" = {
       content  = "route1.mx.cloudflare.net"
       priority = 72
-      comment  = ""
+      comment  = "Used by CloudFlare Email Routing"
     }
     "mx2" = {
       content  = "route2.mx.cloudflare.net"
@@ -63,12 +63,12 @@ locals {
     "cf2024-1._domainkey" = {
       content = "\"v=DKIM1; h=sha256; k=rsa; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAiweykoi+o48IOGuP7GR3X0MOExCUDY/BCRHoWBnh3rChl7WhdyCxW3jgq1daEjPPqoi7sJvdg5hEQVsgVRQP4DcnQDVjGMbASQtrY4WmB1VebF+RPJB2ECPsEDTpeiI5ZyUAwJaVX7r6bznU67g7LvFq35yIo4sdlmtZGV+i0H4cpYH9+3JJ78k\" \"m4KXwaf9xUJCWF6nxeD+qG6Fyruw1Qlbds2r85U9dkNDVAS3gioCvELryh1TxKGiVTkg4wqHTyHfWsp7KD3WQHYJn0RyfJJu6YEmL77zonn7p2SRMvTMP3ZEXibnC9gz3nnhR6wcYL8Q7zXypKTMD58bTixDSJwIDAQAB\""
       name    = "cf2024-1._domainkey"
-      comment = ""
+      comment = "Cloudflare DKIM"
     }
     "spf" = {
       content = "\"v=spf1 include:_spf.mx.cloudflare.net ~all\""
       name    = ""
-      comment = ""
+      comment = "Cloudflare SPF"
     }
     # keep-sorted end
   }
