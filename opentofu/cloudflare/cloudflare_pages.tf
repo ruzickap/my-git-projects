64506e5615171c37d4158f2d7b6a7cc35e6fd936
Petr Ruzicka <petr.ruzicka@gmail.com>
2026-01-02 07:10:12 +0100

    refactor: change Cloudflare Pages project configuration

--- opentofu/cloudflare/cloudflare_pages.tf
+++ opentofu/cloudflare/cloudflare_pages.tf
@@ -1,6 +1,17 @@
+locals {
+  cloudflare_pages_projects = {
+    # keep-sorted start
+    petr_ruzicka_dev   = "petr-ruzicka-dev"
+    ruzickap_github_io = "ruzickap-github-io"
+    xvx_cz             = "xvx-cz"
+    # keep-sorted end
+  }
+}
+
 # Needs: Account - Cloudflare Pages, Project - Cloudflare Pages
-resource "cloudflare_pages_project" "ruzickap_github_io" {
+resource "cloudflare_pages_project" "projects" {
+  for_each          = local.cloudflare_pages_projects
   account_id        = var.cloudflare_account_id
-  name              = "ruzickap-github-io"
+  name              = each.value
   production_branch = "main"
 }
