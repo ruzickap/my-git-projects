3768151532e699c87e8607c810bd80b1f055b3ea
Petr Ruzicka <petr.ruzicka@gmail.com>
2026-02-21 08:46:43 +0100

    chore: format supabase.tf for improved readability

--- opentofu/cloudflare-github/supabase.tf
+++ opentofu/cloudflare-github/supabase.tf
@@ -3,8 +3,9 @@ resource "random_password" "supabase_project_container_image_scans" {
 }
 
 resource "supabase_project" "container_image_scans" {
-  organization_id   = "nejknhtshawlnwtrxyaj"
-  name              = "container-image-scans"
+  organization_id = "nejknhtshawlnwtrxyaj"
+  name            = "container-image-scans"
+  # kics-scan ignore-line
   database_password = random_password.supabase_project_container_image_scans.result
   region            = "us-east-1"
 }
